63d960ba56771f256b4dc9109a3a3fac028460c1
UltimateSoul <44947152+UltimateSoul@users.noreply.github.com>
2019-09-14 19:19:05 +0300

    Initialize

---
+++ TBW2_Forum/production_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'TBWII',
+        'USER': 'Soul',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'}
+
+}
