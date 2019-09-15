b6f890715bd20eff9bd432f21c42cebca157bdb4
UltimateSoul <44947152+UltimateSoul@users.noreply.github.com>
2019-09-15 17:49:32 +0300

    Add login component

---
+++ tbw2-forum-vue/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <button @click="goToLogin">Go to Login Page</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        methods: {
+          goToLogin() {
+            this.$router.push({name: 'login'})
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
