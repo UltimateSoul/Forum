d58355fd0f17cd4ab9133ecc6d209413def8595d
UltimateSoul <44947152+UltimateSoul@users.noreply.github.com>
2019-10-20 21:04:51 +0300

    Move front

---
+++ static/frontend/src/components/pages/GetStarted.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1> Get Started </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GetStarted"
+    }
+</script>
+
+<style scoped>
+
+</style>
